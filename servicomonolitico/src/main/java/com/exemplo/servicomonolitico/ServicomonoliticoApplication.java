cb08a42ff955ccdf3d869f4c758f7038421822e6
Júlio Machado <julioapm@gmail.com>
2024-08-19 14:24:45 -0300

    Setup inicial servicomonolitico

---
+++ servicomonolitico/src/main/java/com/exemplo/servicomonolitico/ServicomonoliticoApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.servicomonolitico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicomonoliticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicomonoliticoApplication.class, args);
+	}
+
+}
---
+++ servicomonolitico/src/test/java/com/exemplo/servicomonolitico/ServicomonoliticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.servicomonolitico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicomonoliticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
