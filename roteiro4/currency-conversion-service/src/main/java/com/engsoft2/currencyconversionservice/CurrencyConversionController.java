f7ed889636bea74db3bc024859ca282617d0b3bf
Júlio Machado <julioapm@gmail.com>
2025-09-26 14:36:56 -0300

    roteiro4 parte1

---
+++ roteiro4/currency-conversion-service/src/main/java/com/engsoft2/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,29 @@
+package com.engsoft2.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyConversionController {
+    private CurrencyExchangeProxy proxy;
+
+    public CurrencyConversionController(CurrencyExchangeProxy proxy) {
+        this.proxy = proxy;
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to,
+            @PathVariable BigDecimal quantity) {
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "feign");
+    }
+
+}
